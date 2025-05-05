5aa161d5687b41326b6e392415577aa56f8cd489
GiovannyGarzón <giovannygarzonsoto@gmail.com>
2025-05-05 14:47:54 -0400

    Updates && base config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common'
-import { CommonModule } from './common/common.module';
-import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config'
+import { CommonModule } from './common/common.module'
+import { SeedModule } from './seed/seed.module'
+import { JoiValidationSchema } from './config/joi.validation'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { EnvConfiguration } from './config/app.config'
+import { join } from 'path'
 
 @Module({
-  imports: [
-  
-  CommonModule,
-  
-  SeedModule],
+  imports: [ConfigModule.forRoot({
+    load: [EnvConfiguration],
+    validationSchema: JoiValidationSchema
+  }),
+  ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'public'),
+  }),
+  MongooseModule.forRoot(process.env.MONGO_DB as string, {
+    dbName: 'supermarket-diff'
+  }),
+  CommonModule, SeedModule],
   controllers: [],
   providers: [],
 })
